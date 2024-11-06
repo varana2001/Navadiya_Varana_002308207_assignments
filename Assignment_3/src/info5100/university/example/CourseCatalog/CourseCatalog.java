81dba139c229531d1753802cc875f7c1d4841312
VaranaNavadiyaNU <navadiya.v@northeastern.edu>
2024-11-05 22:30:30 -0500

    Add 3

--- Assignment_3/src/info5100/university/example/CourseCatalog/CourseCatalog.java
+++ Assignment_3/src/info5100/university/example/CourseCatalog/CourseCatalog.java
@@ -1,43 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package info5100.university.example.CourseCatalog;
 
-import info5100.university.example.Department.Department;
 import java.util.ArrayList;
 
-/**
- *
- * @author kal bugrara
- */
 public class CourseCatalog {
-    Department department;
-    String lastupdated;
-    ArrayList<Course> courselist; 
-    public CourseCatalog(Department d){
-        courselist = new ArrayList();
-        department = d;
+    private ArrayList<Course> courseList;
+
+    public CourseCatalog() {
+        courseList = new ArrayList<>();
     }
-    
-    public ArrayList<Course> getCourseList(){
-        return courselist;
-    }
-    
-    public Course newCourse(String n, String nm, int cr){
-        Course c = new Course(n, nm, cr);
-        courselist.add(c);
+
+    public Course newCourse(String name, String number, int credits, boolean isCore) {
+        Course c = new Course(name, number, credits, isCore);
+        courseList.add(c);
         return c;
     }
-    
-    public Course getCourseByNumber(String n){
-        
-        for( Course c: courselist){
-            
-            if(c.getCOurseNumber().equals(n)) return c;
+
+    public Course getCourseByNumber(String n) {
+        for (Course c : courseList) {
+            if (c.getCourseNumber().equals(n)) {
+                return c;
+            }
         }
         return null;
     }
 
+    public ArrayList<Course> getCourseList() {
+        return courseList;
+    }
+
+    // Method to get all core courses
+    public ArrayList<Course> getCoreCourses() {
+        ArrayList<Course> coreCourses = new ArrayList<>();
+        for (Course c : courseList) {
+            if (c.isCore()) {
+                coreCourses.add(c);
+            }
+        }
+        return coreCourses;
+    }
+
+    // Method to get all elective courses
+    public ArrayList<Course> getElectiveCourses() {
+        ArrayList<Course> electiveCourses = new ArrayList<>();
+        for (Course c : courseList) {
+            if (!c.isCore()) {
+                electiveCourses.add(c);
+            }
+        }
+        return electiveCourses;
+    }
+
+    // Method to print course catalog
+    public void printCourseCatalog() {
+        System.out.println("\n=== COURSE CATALOG ===\n");
+        
+        System.out.println("CORE COURSES:");
+        System.out.println("-------------");
+        for (Course c : getCoreCourses()) {
+            printCourseDetails(c);
+        }
+        
+        System.out.println("\nELECTIVE COURSES:");
+        System.out.println("-----------------");
+        for (Course c : getElectiveCourses()) {
+            printCourseDetails(c);
+        }
+    }
+
+    private void printCourseDetails(Course c) {
+        System.out.printf("%s - %s\n" +
+                         "Credits: %d\n" +
+                         "Cost per Credit: $%,d\n" +
+                         "Total Course Cost: $%,d\n\n",
+            c.getCourseNumber(),
+            c.getName(),
+            c.getCredits(),
+            c.getPricePerCredit(),
+            c.getCoursePrice());
+    }
 }
